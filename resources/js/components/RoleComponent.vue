01da117ea3007586f26bfba31f10d1ffb277d39d
gezrylclariz <gezrylclarizg@gmail.com>
2022-06-25 09:21:27 +0800

    initial component of role and user vue

---
+++ resources/js/components/RoleComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card mb-4">
+        <div class="card-header">Manage Roles</div>
+
+        <div class="card-body">
+            I'm an role manager
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/js/components/UserComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card mb-4">
+        <div class="card-header">Manage Users</div>
+
+        <div class="card-body">
+            I'm an user manager
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
